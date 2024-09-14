06d0b283179356e1f01e73e0d8aa7e074e62196d
Filip Stoklasa <filipstoklasa@Filips-MacBook-Air.local>
2024-09-14 23:09:03 +0300

    f

--- src/components/Drawer/drawer.context.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-import type { UseDisclosureReturn } from "@chakra-ui/react";
-
-export const DrawerContext = createContext<UseDisclosureReturn>(
-  {} as UseDisclosureReturn
-);
-
-export const useDrawer = () => useContext(DrawerContext);
