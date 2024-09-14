e387f5e9c2451bb1f962e9683fee8aa5b1ac58c4
Filip Stoklasa <filipstoklasa@Filips-MacBook-Air.local>
2024-09-14 23:12:38 +0300

    rmvs

--- src/features/Canvas/Canvas.constants.ts
+++
@@ -1,4 +0,0 @@
-import type { Marker } from "@/global-context/config/config.types";
-
-export const CANVAS_STATE = new Map<string, Marker>();
-export const ERASER: Marker = { color: "", symbol: "" };
